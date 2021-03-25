d3c7484121887a4bb7dee087e9e9ff2c281304b6
Jess Dever <jessicadever1@gmail.com>
2021-03-25 10:32:49 -0500

    initial

---
+++ DogGo/Repository/IWalkerRepository.cs
@@ -0,0 +1,12 @@
+﻿using DogGo.Models; //gives us access to the Models directory
+using Microsoft.Data.SqlClient; //need clarification on what this provides
+using System.Collections.Generic; //allows us to work with lists
+
+namespace DogGo.Repositories //This namespace allows us to work with other files in the Repo directory
+{
+    public interface IWalkerRepository //Interface is like: this is what I'm bringing to the table, and allows us to cherry-pick THINGS (methods, properties)
+    {
+        List<Walker> GetAllWalkers(); //This a method used to GET a list of walkers from the database
+        Walker GetWalkerById(int id); //This a method used to select a specific walker by their Id?
+    }
+}
---
+++ DogGo/Repository/WalkerRepository.cs
@@ -0,0 +1,101 @@
+﻿using DogGo.Models; //This gives us access to the classes in the Models folder
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic; //This makes it possible to work with Lists
+
+namespace DogGo.Repositories //This says which portion of the DogGo app we're in (Reps), and gives us access to the other classes in Reps
+{
+    public class WalkerRepository : IWalkerRepository //This names (declares?) a public (accessible outside of this space) class, and uses properties from the associated Interface
+    {
+        private readonly IConfiguration _config;
+
+        // The constructor accepts an IConfiguration object as a parameter. This class comes from the ASP.NET framework and is useful for retrieving things out of the appsettings.json file like connection strings.
+        // We can tell it's a constructor bc it doesn't have a return, and it's named the same thing as the file
+        public WalkerRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public List<Walker> GetAllWalkers()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, [Name], ImageUrl, NeighborhoodId
+                        FROM Walker
+                    ";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Walker> walkers = new List<Walker>();
+                    while (reader.Read())
+                    {
+                        Walker walker = new Walker
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            ImageUrl = reader.GetString(reader.GetOrdinal("ImageUrl")),
+                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId"))
+                        };
+
+                        walkers.Add(walker);
+                    }
+
+                    reader.Close();
+
+                    return walkers;
+                }
+            }
+        }
+
+        public Walker GetWalkerById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, [Name], ImageUrl, NeighborhoodId
+                        FROM Walker
+                        WHERE Id = @id
+                    ";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        Walker walker = new Walker
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            ImageUrl = reader.GetString(reader.GetOrdinal("ImageUrl")),
+                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId"))
+                        };
+
+                        reader.Close();
+                        return walker;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        return null;
+                    }
+                }
+            }
+        }
+    }
+}
