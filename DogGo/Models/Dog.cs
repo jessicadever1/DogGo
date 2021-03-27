9c3184a0de4fa1ca3a54d55457a1bb4d4fdab43f
Jess Dever <jessicadever1@gmail.com>
2021-03-27 13:11:46 -0500

    added dog.cs

---
+++ DogGo/Models/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Models
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public int OwnerId { get; set; }
+        public string Breed { get; set; }
+        public string Notes { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
