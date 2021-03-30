748f0ad07664206d1679ab963313930946188d6d
Jess Dever <jessicadever1@gmail.com>
2021-03-30 16:02:10 -0500

    chpt 4

---
+++ DogGo/Repository/IWalkRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogGo.Models;
+
+namespace DogGo.Repository
+{
+    public class IWalkRepository
+    {
+        List<Walks> GetAllWalks(); //This a method used to GET a list of walks from the database
+        Walks GetWalkById(int id);
+    }
+}
